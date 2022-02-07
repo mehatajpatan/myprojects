9f4e39c736b37cabd5bd6cd0d176d2e3259d24eb
mehatajpatan <85663898+mehatajpatan@users.noreply.github.com>
2022-02-07 13:17:44 +0530

    Add files via upload

---
+++ finallyblock.java
@@ -0,0 +1,24 @@
+package keywordsnexceptions;
+
+public class FinallyBlock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=45,b=0,rs=0;
+        try
+        {
+            rs = a / b;
+        }
+        catch(ArithmeticException Ex)
+        {
+            System.out.print("\n\tError : " + Ex.getMessage());
+        }
+        finally
+        {
+            System.out.print("\n\tThe result is : " + rs);
+        }
+    }
+
+	}
+
+
